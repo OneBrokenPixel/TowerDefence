dbbec8bbfc0d4c367ed1435044bb262bd0d735dd
Anthony Todd <anthonytodd@Anthonys-MacBook-Air.local>
2014-03-17 20:46:02 +0000

    commiting to merge with git ignore

---
+++ Assets/Scripts/AI/AI_Wave.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[AddComponentMenu("Scripts/AI/Wave")]
+public class AI_Wave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
