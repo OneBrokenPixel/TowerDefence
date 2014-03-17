0c6840aba5d4a920b4246f43429ac23b5beddd91
Anthony Todd <anthonytodd@Anthonys-MacBook-Air.local>
2014-03-17 20:35:41 +0000

    Added Simple Pools to project Created 3 ai prefavs Created Spawners for all three prefabs Created AI node in the heriarchy.

---
+++ Assets/Scripts/AI/AI_March.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Hostile.SimplePool.Components;
+
+[AddComponentMenu("Scripts/AI/March")]
+public class AI_March : BasePoolComponent {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	#region implemented abstract members of BasePoolComponent
+
+	public override void OnSpawn ()
+	{
+		transform.rotation = Quaternion.Euler(0f,0f,180f);
+		rigidbody2D.AddForce(transform.right * speed);
+	}
+
+	public override void OnDespawn ()
+	{
+
+	}
+
+	#endregion
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+}
