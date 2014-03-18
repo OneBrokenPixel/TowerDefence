56d2b04016736bc6f03ef9507c7b6418e28e164b
Rieya <rieya.online@gmail.com>
2014-03-18 15:28:47 +0000

    gun fires

--- Assets/PlayerTankScript.cs
+++ Assets/PlayerTankScript.cs
@@ -5,16 +5,20 @@ public class PlayerTankScript : MonoBehaviour {
 
     public GameObject gun;
     public GameObject body;
+    public GameObject bullet;
+
+    private float fireRate = 0.3f;
+    private float nextFire = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
           
-         //rotation
+         //rotation, follow mouse pointer
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
  
@@ -25,5 +29,15 @@ public class PlayerTankScript : MonoBehaviour {
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         gun.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-	    }
+        // fire towards mouse
+        //if (Input.GetMouseButtonDown(0)) {
+        nextFire += Time.deltaTime;
+        if (nextFire >= fireRate) {
+            GameObject b = Instantiate(bullet, gun.transform.position+(1.3f*gun.transform.right), gun.transform.rotation) as GameObject;
+            b.rigidbody2D.velocity = b.transform.right * 4;
+            nextFire = 0.0f;
+        }
+
+
+    }
 }
