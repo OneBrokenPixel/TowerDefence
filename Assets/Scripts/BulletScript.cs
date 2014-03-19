163c97a215c21acd990e57e371324f82d536eca0
Anthony Todd <toddy201@gmail.com>
2014-03-19 17:36:05 +0000

    Added new collison layer and moved hit resoution to the bullet.

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+    /*
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(gameObject);
+    }
+    */
+
+    public int damage = 1;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        AI_March ai = coll.gameObject.GetComponent<AI_March>();
+
+        if (ai != null)
+        {
+            ai.Hit(damage, this.transform.position - coll.transform.position );
+        }
+
+        Destroy(gameObject);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
