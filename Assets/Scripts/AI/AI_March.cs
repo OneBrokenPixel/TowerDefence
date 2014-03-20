d470605b6fd16e550065a762a0ac97c269f82e35
Anthony Todd <toddy201@gmail.com>
2014-03-20 17:39:31 +0000

    Mosters now use A*

--- Assets/Scripts/AI/AI_March.cs
+++ Assets/Scripts/AI/AI_March.cs
@@ -4,6 +4,8 @@ using System.Collections;
 using Hostile.SimplePool.Components;
 using Hostile.SimplePool;
 
+using Pathfinding;
+
 [AddComponentMenu("Scripts/AI/March")]
 public class AI_March : BasePoolComponent {
 
@@ -15,6 +17,13 @@ public class AI_March : BasePoolComponent {
 
     public SimplePool splatPool;
     public GameObject splatGO;
+
+    private GameObject _player;
+
+    Seeker _seeker;
+    Path _path;
+    int _currentWaypoint = 0;
+    public float nextWaypointDistance = 3;
 
     float _scale = 1f;
 
@@ -28,14 +37,25 @@ public class AI_March : BasePoolComponent {
 
 	public override void OnSpawn ()
 	{
+
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _seeker = GetComponent<Seeker>();
+        _seeker.StartPath(transform.position, _player.transform.position, OnPathComplete);
         _hitpoints = hitPoints;
         _scale = 1f;
-		//Debug.Log("adding force");
-		//rigidbody2D.WakeUp();
-		//rigidbody2D.AddForce(transform.right * speed);
 
         _targetVel = transform.right * speed;
 	}
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Yey, we got a path back. Did it have an error? " + p.error);
+        if (!p.error)
+        {
+            _path = p;
+            _currentWaypoint = 0;
+        }
+    }
 
 	public override void OnDespawn ()
 	{
@@ -107,9 +127,35 @@ public class AI_March : BasePoolComponent {
     }
 
 	// Update is called once per frame
-	void FixedUpdate () {
+    public void FixedUpdate()
+    {
+        if (_path == null)
+        {
+            //We have no path to move after yet
+            return;
+        }
+
+        if (_currentWaypoint >= _path.vectorPath.Count)
+        {
+            Debug.Log("End Of Path Reached");
+            return;
+        }
+
+        //Direction to the next waypoint
+        Vector3 dir = (_path.vectorPath[_currentWaypoint] - transform.position).normalized;
+        dir *= speed;
+        _targetVel.Set(dir.x, dir.y);
+
+            
         Vector2 diffVel = _targetVel - rigidbody2D.velocity;
         rigidbody2D.AddForce(diffVel);
-        
-	}
+
+        //Check if we are close enough to the next waypoint
+        //If we are, proceed to follow the next waypoint
+        if (Vector3.Distance(transform.position, _path.vectorPath[_currentWaypoint]) < nextWaypointDistance)
+        {
+            _currentWaypoint++;
+            return;
+        }
+    }
 }
